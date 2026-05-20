2ffc6a4ac50a76f23b5a90bf3cb661ae9cc6989e
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-20 13:04:21 +0200

    Add docker1 VM resource

---
+++ proxmox-core/docker1.tf
@@ -0,0 +1,67 @@
+resource "proxmox_virtual_environment_vm" "docker1" {
+  vm_id         = 9030
+  name          = "docker1"
+  description   = "Docker Compose services host managed by Terraform"
+  node_name     = var.target_node_name
+  tags          = ["terraform", "ubuntu", "docker"]
+  on_boot       = true
+  started       = true
+  scsi_hardware = "virtio-scsi-single"
+
+  cpu {
+    cores = 8
+    type  = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated = 32768
+  }
+
+  agent {
+    enabled = true
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  network_device {
+    bridge  = var.vm_bridge
+    vlan_id = 12
+  }
+
+  disk {
+    datastore_id = var.target_storage
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_noble_cloud_image.id
+    interface    = "scsi0"
+    size         = 500
+    iothread     = true
+    discard      = "on"
+  }
+
+  initialization {
+    datastore_id = var.target_storage
+
+    dns {
+      servers = var.vm_dns_servers
+    }
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      username = var.vm_username
+      keys     = var.vm_ssh_public_keys
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disk[0].file_id,
+      node_name,
+    ]
+  }
+}
