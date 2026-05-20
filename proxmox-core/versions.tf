ef4d647aff0940fb117928eedc5b758c440a6224
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-20 21:39:04 +0200

    Use HCP Terraform workspace for proxmox core state

--- proxmox-core/versions.tf
+++ proxmox-core/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  cloud {
+    workspaces {
+      name = "terraform-proxmox-core"
+    }
+  }
+
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
