e8df85607da5b42738f66c388148626af5c4368a
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-20 21:32:19 +0200

    Attach base cloud-init snippet to mkdocs

--- proxmox-core/mkdocs.tf
+++ proxmox-core/mkdocs.tf
@@ -40,7 +40,8 @@ resource "proxmox_virtual_environment_vm" "mkdocs" {
   }
 
   initialization {
-    datastore_id = var.target_storage
+    datastore_id       = var.target_storage
+    user_data_file_id = proxmox_virtual_environment_file.noble_base_cloud_config.id
 
     dns {
       servers = var.vm_dns_servers
@@ -54,7 +55,7 @@ resource "proxmox_virtual_environment_vm" "mkdocs" {
 
     user_account {
       username = var.vm_username
-      keys     = var.vm_ssh_public_keys
+      keys     = local.default_ssh_public_keys
     }
   }
 
