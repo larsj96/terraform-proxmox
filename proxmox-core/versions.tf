c4542126208c37f6b2828ba331b8138aff77607f
Lars-Jørgen <46743467+larsj96@users.noreply.github.com>
2026-05-20 22:00:44 +0200

    Use S3-compatible backend for proxmox state

--- proxmox-core/versions.tf
+++ proxmox-core/versions.tf
@@ -1,11 +1,7 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.10.0"
 
-  cloud {
-    workspaces {
-      name = "terraform-proxmox-core"
-    }
-  }
+  backend "s3" {}
 
   required_providers {
     proxmox = {
